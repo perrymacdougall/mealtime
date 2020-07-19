e1c912103366e8748622098d41bd75349cb7f91e
perrymacdougall <perrymacdougall@gmail.com>
2020-07-19 13:31:53 -0400

    added params to display selected content from dummy data

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+import Category from '../models/category';
+
 const CategoryMealsScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen!</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Grab the Meal Deets!" onPress={() => {
                 props.navigation.navigate({routeName: 'MealDetail'})
             }} />
