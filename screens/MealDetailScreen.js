70e8f2701c4d2665bae5cfb1da77542b18d87634
perrymacdougall <perrymacdougall@gmail.com>
2020-07-26 14:27:36 -0400

    created a custom HeaderButton component to mark meals as favourites

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
     const mealId = props.navigation.getParam('mealId');
@@ -19,9 +21,20 @@ MealDetailScreen.navigationOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title
+        headerTitle: selectedMeal.title,
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item 
+                    title='Favourite' 
+                    iconName='ios-star' 
+                    onPress={() => {
+                        console.log('Mark as favourite');
+                    }}
+                />
+            </HeaderButtons>
+        )
     };
-}
+};
 
 const styles= StyleSheet.create({
     screen: {
